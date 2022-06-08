f21fcb1dd76098521d858e53af7f2a9daa82d857
Rustin1 <92025519+Rustin1@users.noreply.github.com>
2022-06-08 20:58:29 +0200

    Add files via upload

---
+++ enum marki telewizorów.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+enum Telewizor{Samsung,LG,Apple};
+
+int main()
+{
+    enum Telewizor tab[6] = {Samsung,Samsung,LG,LG,LG,Apple};
+    for(int i=0;i<6;i++){
+        if(tab[i]==1){
+            printf("Samsung\n");
+        }
+    }
+    return 0;
+}
