d1274ec97f491bd6f820d88264109c5a81c71dda
wilduk <80290557+wilduk@users.noreply.github.com>
2022-06-09 12:01:20 +0200

    Create 111 2.c

---
+++ 111 2.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+const char *samogloski = "aeiouy";
+
+int samogloska(char litera){
+	for(int i = 0;i<strlen(samogloski);i++){
+		if(litera == samogloski[i] || litera == samogloski[i]-97+65){
+			return 1;
+		}
+	}
+	return 0;
+}
+
+int foo(char * napis){
+	int dl = strlen(napis);
+	int suma = 0;
+	for(int i = 0;i<dl;i++){
+		if(samogloska(napis[i]) == 0){
+			suma += 1;
+		}
+	}
+	return suma;
+}
+
+int main()
+{
+	char* napis = "Ala Ma Kota";
+	printf("%d",foo(napis));
+}
