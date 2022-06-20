b9fc48758dde663cf998598b7098b765367a9c44
Rustin1 <92025519+Rustin1@users.noreply.github.com>
2022-06-20 15:49:56 +0200

    Update Rekurencja.c

--- Rekurencja.c
+++ Rekurencja.c
@@ -5,7 +5,9 @@ int rek(n){
 	if(n==0){
 		return 4; //start ciągu
 	}
-	//else if(n==1) jeśli ciąg ma więcej wartości na starcie np a0 = 1 i a1 = 5
+	else if(n==1){
+                return 5;
+        }
 	return rek(n-1)+4; //jeśli nie jest to wartość początkowa to zrob działanie (np +) na poprzedniej wartości z ciągu i liczbie (np 4 lub n)
 }
 
